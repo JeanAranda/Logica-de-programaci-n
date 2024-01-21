f1b2834c51c539da770eb72d40ebd78d1140c849
JeanAranda <jarandar2014@gmail.com>
2024-01-21 18:08:41 -0500

    avance de loops y while

--- app.js
+++ app.js
@@ -3,13 +3,26 @@
  esta es otra forma de usar un comentario
  */
 let numeroSecreto = 6;
-let numeroUsuario = prompt('Me indicas el número porfavor:');
-console.log(numeroUsuario);
-
-if (numeroUsuario == numeroSecreto){
-    //Acertamos
-    alert(`Acertaste el número secreto es6: ${numeroUsuario}`)
-} else {
-    //No se pudo acertar aun
-    alert('No acertaste el número');
+let numeroUsuario = 0;
+let intentos = 1;
+let palabaraVeces = 'vez';
+while (numeroUsuario !=  numeroSecreto) {
+    numeroUsuario = prompt('Me indicas el número porfavor:');
+    console.log(numeroUsuario);
+    
+    if (numeroUsuario == numeroSecreto){
+        //Acertamos
+        alert(`Acertaste el número secreto es: ${numeroUsuario}, lo hiciste en ${intentos} ${palabaraVeces}`)
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El numero secreto es menor');
+        }else{
+            alert('El numero secreto es mayor');
+        }
+        //Se incrementa el contador en uno cuando no se acierta
+        intentos = intentos + 1;
+        palabaraVeces = 'veces';
+        //No se pudo acertar aun
+        //alert('No acertaste el número');
+    }
 }
