4940e4243700a7c40d8a521113511e850b15e2dd
JeanAranda <jarandar2014@gmail.com>
2024-01-22 21:02:03 -0500

    Se actualiza hasta el codigo ternario

--- app.js
+++ app.js
@@ -5,14 +5,15 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabaraVeces = 'vez';
+//let palabaraVeces = 'vez';
+let maximosIntentos = 3;
 while (numeroUsuario !=  numeroSecreto) {
     numeroUsuario = prompt('Me indicas el número porfavor:');
     console.log(numeroUsuario);
     
     if (numeroUsuario == numeroSecreto){
         //Acertamos
-        alert(`Acertaste el número secreto es: ${numeroUsuario}, lo hiciste en ${intentos} ${palabaraVeces}`)
+        alert(`Acertaste el número secreto es: ${numeroUsuario}, lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El numero secreto es menor');
@@ -20,8 +21,13 @@ while (numeroUsuario !=  numeroSecreto) {
             alert('El numero secreto es mayor');
         }
         //Se incrementa el contador en uno cuando no se acierta
-        intentos = intentos + 1;
-        palabaraVeces = 'veces';
+        //intentos = intentos + 1;
+        //palabaraVeces = 'veces';
+        intentos ++;
+        if (intentos > maximosIntentos){
+            alert(`Llegaste al número maximo de ${maximosIntentos} intentos`);
+            break;
+        }
         //No se pudo acertar aun
         //alert('No acertaste el número');
     }
